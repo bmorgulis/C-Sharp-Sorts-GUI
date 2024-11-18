ea47f01d9b518192709ed78f3a50a1b2bbbd8eb6
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2024-11-18 14:09:11 -0500

    Shell sort algorithm

--- SortVisualization2/Algorithms/ShellSort.cs
+++ SortVisualization2/Algorithms/ShellSort.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using SortVisualization2.Entity;
 
 namespace SortVisualization2.Algorithms
 {
     public class ShellSort
     {
+       
+        public void ShellSortAlgorithm(List<Element> elements, Graphics graphic)
+        {
+            int size = elements.Count;
+            for (int gap = size / 2; gap > 0; gap = gap / 2) //reduce the gap by half each time. The last gap will be 1. 
+            {
+                for(int i = gap; i < size; i++) //start from the gap because the elements before the gap are already sorted
+                {
+                    Element temp = elements[i]; //the element we are going to insert into the sorted part of the array
+                    int j;
+                    for (j = i; j >= gap && elements[j - gap].Value > temp.Value; j = j - gap)
+                    {
+                        elements[j] = elements[j - gap];
+                    }
+                    elements[j] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+    for (int i = gap; i < size; i++)
+    {
+        int temp = elements[i];
+        int j;
+        for (j = i; j >= gap && elements[j - gap] > temp; j -= gap)
+        {
+            elements[j] = elements[j - gap];
+        }
+        elements[j] = temp;
     }
 }
